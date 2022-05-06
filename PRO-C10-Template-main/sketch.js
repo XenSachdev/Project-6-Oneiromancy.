27937502cac6b963de6907cabe1aa343991f1e3f
XenSachdev <99481741+XenSachdev@users.noreply.github.com>
2022-05-06 17:21:58 +0530

    Update sketch.js

--- PRO-C10-Template-main/sketch.js
+++ PRO-C10-Template-main/sketch.js
@@ -6,9 +6,9 @@ function preload(){
 
   hipImg1 = loadAnimation("ship-1.png");
   shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png");
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   
   seaImg = loadImage("sea.png");
 }
